254dc50190f52906fcb97844f70fc22efa7abf78
Temuujin <temuujin.natsagnyam@gmail.com>
2020-06-14 19:15:04 +0100

    extensions pattern implemented

--- sanzai/app.py
+++ sanzai/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from sanzai.blueprints.page import page
 from sanzai.blueprints.product import product
 from sanzai.blueprints.cart import cart
+from sanzai.extensions import debug_toolbar
 
 
 def create_app(settings_override=None):
@@ -24,4 +25,18 @@ def create_app(settings_override=None):
     app.register_blueprint(product, url_prefix='/delguur')
     app.register_blueprint(cart, url_prefix='/sags')
 
+    extensions(app)
+
     return app
+
+
+def extensions(app):
+    """
+    Register 0 or more extensions (mutates the app passed in).
+
+    :param app: Flask application instance
+    :return: None
+    """
+    debug_toolbar.init_app(app)
+
+    return None
