179bd6558cc4a703d05094753ff74025da80de9c
Temuujin <temuujin.natsagnyam@gmail.com>
2020-06-18 22:30:51 +0100

    test amendment

--- sanzai/tests/contact/test_views.py
+++ sanzai/tests/contact/test_views.py
@@ -1,4 +1,4 @@
-from flask import url_for, jsonify
+from flask import url_for
 
 from lib.tests import assert_status_with_message
 
@@ -8,3 +8,14 @@ class TestContact(object):
         """ Contact page should respond with a success 200. """
         response = client.get(url_for('contact.index'))
         assert response.status_code == 200
+
+    def test_contact_form(self, client):
+        """ Contact form should redirect with a message. """
+        form = {
+          'email': 'foo@bar.com',
+          'message': 'Test message from Sanzai.',
+        }
+
+        response = client.post(url_for('contact.index'), data=form,
+                               follow_redirects=True)
+        assert_status_with_message(200, response, 'Thank')
